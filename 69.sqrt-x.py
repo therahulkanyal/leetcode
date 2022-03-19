a86fd3f45ef8876a5a73a69c6e990a3d2fd6c874
Rahul Kanyal <therahulkanyal@gmail.com>
2022-03-19 16:06:04 +0530

    added code for sqrt

---
+++ 69.sqrt-x.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=69 lang=python3
+#
+# [69] Sqrt(x)
+#
+
+# @lc code=start
+class Solution(object):
+    def mySqrt(self, x):
+        l, r = 0, x
+        while l <= r:
+            mid = l + (r-l)//2
+            if mid * mid <= x < (mid+1)*(mid+1):
+                return mid
+            elif x < mid * mid:
+                r = mid - 1
+            else:
+                l = mid + 1
+# @lc code=end
+
